ff8cd983b85a8554de94f059b5834294c2fcb43e
Rajat Rathor <rajat.kumar@scriza.in>
2026-02-05 15:08:08 +0530

    Fix redis CommonJS import

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,8 +1,8 @@
-import Redis from "ioredis";
+const Redis = require("ioredis");
 
 const redis = new Redis({
     host: "redis",
     port: 6379,
 });
 
-export default redis;
+module.exports = redis;
